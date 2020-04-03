cb527f068378823c3fa8626f2d7109f46710a5db
Gremple <56596090+Gremple@users.noreply.github.com>
2020-04-03 11:35:17 +0200

    Test commit
    nur ein Kommentar

--- CustomCocktails.cpp
+++ CustomCocktails.cpp
@@ -3,6 +3,7 @@ Type:		HTML Index Page
 Date:		14.08.2019
 Author:		Tim Schanz
 Description:	This file stores the dynamic HTML content of the configuration page.
+Last change: 20200403; 11:34:38
 */
 
 #include "CustomCocktails.h"
